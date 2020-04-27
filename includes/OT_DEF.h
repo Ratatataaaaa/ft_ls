acf1c67cd16cb0d7e8739447406b4f1864e329b9
Raa <Raa@MacBook-Pro-Roman.local>
2020-04-27 18:41:04 +0300

    time

--- includes/OT_DEF.h
+++ includes/OT_DEF.h
@@ -6,7 +6,7 @@
 /*   By: cwing <cwing@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/24 20:22:41 by cwing             #+#    #+#             */
-/*   Updated: 2020/04/27 18:00:00 by cwing            ###   ########.fr       */
+/*   Updated: 2020/04/27 18:40:55 by cwing            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 # define YWL_T		"\x1b[33m"
 # define STD_T		"\x1b[0m"
 
-# ifndef __APPLE__
-	st_atimespec st_atime
-	st_ctimespec st_ctime 
-	st_mtimespec st_mtime
-# endif
+#ifndef _POSIX_SOURCE
+     #define st_atime st_atimespec.tv_sec
+     #define st_mtime st_mtimespec.tv_sec
+     #define st_ctime st_ctimespec.tv_sec
+#endif
 
 #endif
