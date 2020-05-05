93c0ea7a9e504fba7f63d1b7f41c6a4762ad0ccb
Liza <liza_ryzhkina@hotmail.com>
2020-05-05 14:17:26 +0300

    bonus flag F

---
+++ F_flag.c
@@ -0,0 +1,10 @@
+#include "../includes/FT_LS.h"
+
+void	F_print(t_dir *head)
+{
+	struct stat buf;
+	if (head->chmod[0] == 'd')
+		ft_putstr("/");
+	else if (stat(head->name, &buf) == 0 && buf.st_mode & S_IXUSR)
+		ft_putchar('*');
+}
