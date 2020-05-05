8ce0ae75207bd72fc16d03570529635473dd9d0a
Liza <liza_ryzhkina@hotmail.com>
2020-05-05 18:50:17 +0300

    bez stat

--- src/F_flag.c
+++ src/F_flag.c
@@ -4,8 +4,8 @@ void	F_print(t_dir *head)
 {
 	struct stat buf;
 	if (head->chmod[0] == 'd')
-		ft_putstr("/");
-	else if (stat(head->name, &buf) == 0 && buf.st_mode & S_IXUSR)
+		ft_putchar('/');
+	else if ((int)head->chmod & S_IXUSR)
 		ft_putchar('*');
 	else if (head->chmod[0] == 'l')
 		ft_putchar('@');
