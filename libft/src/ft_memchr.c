8a677f10fbe92d585e1886134ca27a13fe3c96a1
Raa <Raa@MacBook-Pro-Roman.local>
2020-05-07 20:48:31 +0300

    good luck

--- libft/src/ft_memchr.c
+++ libft/src/ft_memchr.c
@@ -3,28 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bdrinkin <bdrinkin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cwing <cwing@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/13 21:52:59 by bdrinkin          #+#    #+#             */
-/*   Updated: 2019/11/04 23:07:07 by bdrinkin         ###   ########.fr       */
+/*   Created: 2019/09/13 21:52:59 by cwing             #+#    #+#             */
+/*   Updated: 2020/05/07 20:38:39 by cwing            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void				*ft_memchr(const void *s, int c, size_t n)
+void					*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
+	unsigned const char	*temp;
+	size_t				iter;
 
-	i = 0;
-	if (n == 0 || (n + 1 < n))
-		return (NULL);
-	while (n != 0)
+	temp = s;
+	iter = 0;
+	while (iter < n)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *)&(s)[i]);
-		n--;
-		i++;
+		if (temp[iter] == (unsigned char)c)
+			return ((void*)(temp + iter));
+		else
+			iter++;
 	}
 	return (NULL);
 }
